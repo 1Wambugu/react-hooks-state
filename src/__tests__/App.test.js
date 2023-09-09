ae1df4d470a405022e0ceefc1658f461b52094db
Martin <martingakuya@gmail.com>
2023-09-10 01:05:10 +0300

    completed

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,5 +1,10 @@
 test("Test passing", () => {
   return new Promise((resolve, reject) => {
     resolve(true);
+    reject("Test failed");
+  }).then((result) => {
+    expect(result).toBe(true); 
+  }).catch((error) => {
+    expect(error).toBeUndefined();
   });
 });
